059a7ea496f3a2db39129a6c7e83df4a89c5574e
KelseyJ22 <kelsey2@stanford.edu>
2017-12-28 10:19:32 -0800

    save compressed images as csvs

--- image_compression.py
+++ image_compression.py
@@ -3,17 +3,20 @@ from PIL import Image
 import scipy.misc as smp
 import utils
 
-k_vals = [1, 5, 10, 20]
-
+k_vals = [10, 20]
+			
 def compress():
 	images, _ = utils.read_from_csv(False, './data/fashion-mnist_train.csv')
-	for i in range(0, images.shape[0]):
-		img = np.reshape(images[i], (28,28))
-		u, d, v = np.linalg.svd(img, full_matrices=True, compute_uv=True)
-		d = np.diag(d)
-		#print(img.shape)
+	for k in k_vals:
+		print 'k:', k
+		compressed = np.zeros(images.shape)
+		for i in range(0, images.shape[0]):
+			if i % 500 == 0:
+				print i
+			img = np.reshape(images[i], (28,28))
+			u, d, v = np.linalg.svd(img, full_matrices=True, compute_uv=True)
+			d = np.diag(d)
 
-		for k in k_vals:
 			uk = u[:,:k] # first k columns of U (m x m becomes m x k)
 			dk = d[:k,:k] # first k rows and columns of d (m x n becomes k x k)
 			vk = v[:k,] # first k rows of Vt (n x n becomes k x n)
@@ -21,8 +24,12 @@ def compress():
 			k_approx = np.matmul(np.matmul(uk, dk), vk)
 			k_approx_rescaled = (k_approx - np.min(k_approx))/(np.max(k_approx) - np.min(k_approx)) # rescale values to fit between 0 and 1
 			shrunk = np.uint8(k_approx_rescaled*255)
+			#print(shrunk.shape)
 			#im = Image.fromarray(shrunk)
 			#im.show()
-			# TODO: save compressed images to CSV
+			to_save = np.reshape(shrunk, (1, 784))
+			compressed[i:] = to_save
+		numpy.savetxt(str(k) + '.csv', compressed, delimiter=',')
+
 
 compress()
